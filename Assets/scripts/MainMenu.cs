5192ac8a45a51928d028651ab072fd653ca71d91
zk <zombitrafik05@mail.ru>
2014-07-19 20:42:10 +0300

    ICO
    Обновлено главное меню

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -30,30 +30,26 @@ public class MainMenu : MonoBehaviour {
 
 	public void ShowBackMenu()
 	{
+		playCopy = (GameObject)Instantiate(play, new Vector3(8, 4, -5), Quaternion.identity);
+		modeCopy = (GameObject)Instantiate(mode, new Vector3(8, 2, -5), Quaternion.identity);
+		settingsCopy = (GameObject)Instantiate(settings, new Vector3(8, 0, -5), Quaternion.identity);
+		tutorialCopy = (GameObject)Instantiate(tutorial, new Vector3(8, -2, -5), Quaternion.identity);
+		aboutCopy = (GameObject)Instantiate(about, new Vector3(8, -4, -5), Quaternion.identity);
+		exitCopy = (GameObject)Instantiate(exit, new Vector3(8, -6, -5), Quaternion.identity);
 
-		playCopy = (GameObject)Instantiate(play, new Vector3(8, 6, -5), Quaternion.identity);
-		modeCopy = (GameObject)Instantiate(mode, new Vector3(8, 4, -5), Quaternion.identity);
-		settingsCopy = (GameObject)Instantiate(settings, new Vector3(8, 2, -5), Quaternion.identity);
-		tutorialCopy = (GameObject)Instantiate(tutorial, new Vector3(8, 0, -5), Quaternion.identity);
-		aboutCopy = (GameObject)Instantiate(about, new Vector3(8, -2, -5), Quaternion.identity);
-		exitCopy = (GameObject)Instantiate(exit, new Vector3(8, -4, -5), Quaternion.identity);
+		iTween.MoveTo(playCopy, iTween.Hash("position", new Vector3(-2, 4, -5), "time", 0.5f, "delay", 0.5f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(modeCopy, iTween.Hash("position", new Vector3(-1.5f, 2, -5), "time", 0.5f, "delay", 0.52f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(settingsCopy, iTween.Hash("position", new Vector3(-1f, 0, -5), "time", 0.5f, "delay", 0.54f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(tutorialCopy, iTween.Hash("position", new Vector3(-0.5f, -2, -5), "time", 0.5f, "delay", 0.56f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(aboutCopy, iTween.Hash("position", new Vector3(0, -4, -5), "time", 0.5f, "delay", 0.58f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(exitCopy, iTween.Hash("position", new Vector3(0.5f, -6, -5), "time", 0.5f, "delay", 0.6f, "easetype", iTween.EaseType.easeOutSine));
 
-
-		iTween.MoveTo(playCopy, iTween.Hash("position", new Vector3(-2, 6, -5), "time", 0.5f, "delay", 0.5f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(modeCopy, iTween.Hash("position", new Vector3(-1.5f, 4, -5), "time", 0.5f, "delay", 0.52f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(settingsCopy, iTween.Hash("position", new Vector3(-1f, 2, -5), "time", 0.5f, "delay", 0.54f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(tutorialCopy, iTween.Hash("position", new Vector3(-0.5f, 0, -5), "time", 0.5f, "delay", 0.56f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(aboutCopy, iTween.Hash("position", new Vector3(0, -2, -5), "time", 0.5f, "delay", 0.58f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(exitCopy, iTween.Hash("position", new Vector3(0.5f, -4, -5), "time", 0.5f, "delay", 0.6f, "easetype", iTween.EaseType.easeOutSine));
-
-
-		iTween.MoveTo(playCopy, iTween.Hash("position", new Vector3(-1.6f, 6, -5), "time", 0.2f, "delay", 1f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(modeCopy, iTween.Hash("position", new Vector3(-1.1f, 4, -5), "time", 0.2f, "delay", 1.02f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(settingsCopy, iTween.Hash("position", new Vector3(-0.6f, 2, -5), "time", 0.2f, "delay", 1.04f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(tutorialCopy, iTween.Hash("position", new Vector3(-0.1f, 0, -5), "time", 0.2f, "delay", 1.06f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(aboutCopy, iTween.Hash("position", new Vector3(0.4f, -2, -5), "time", 0.2f, "delay", 1.08f, "easetype", iTween.EaseType.easeOutSine));
-		iTween.MoveTo(exitCopy, iTween.Hash("position", new Vector3(0.9f, -4, -5), "time", 0.2f, "delay", 1.1f, "easetype", iTween.EaseType.easeOutSine));
-
+		iTween.MoveTo(playCopy, iTween.Hash("position", new Vector3(-1.6f, 4, -5), "time", 0.2f, "delay", 1f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(modeCopy, iTween.Hash("position", new Vector3(-1.1f, 2, -5), "time", 0.2f, "delay", 1.02f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(settingsCopy, iTween.Hash("position", new Vector3(-0.6f, 0, -5), "time", 0.2f, "delay", 1.04f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(tutorialCopy, iTween.Hash("position", new Vector3(-0.1f, -2, -5), "time", 0.2f, "delay", 1.06f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(aboutCopy, iTween.Hash("position", new Vector3(0.4f, -4, -5), "time", 0.2f, "delay", 1.08f, "easetype", iTween.EaseType.easeOutSine));
+		iTween.MoveTo(exitCopy, iTween.Hash("position", new Vector3(0.9f, -6, -5), "time", 0.2f, "delay", 1.1f, "easetype", iTween.EaseType.easeOutSine));
 	}
 
 	void Update()
